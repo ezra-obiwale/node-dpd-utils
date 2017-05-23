bd19ec401ee6533be81629bb3cfeee0cfb8d210a
Ezra Obiwale <contact@ezraobiwale.com>
2017-05-23 18:57:03 +0100

    Added method mongoCollection

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 module.exports = {
+    /**
+     * Parse the given content with the given variables
+     * @param {string} content
+     * @param {object} variables
+     * @returns {string}
+     */
     parseTemplate: function (content, variables) {
         for (var key in variables) {
             content = content.replace(new RegExp('{{' + key + '}}', 'g'), variables[key]);
         }
         return content;
+    },
+    /**
+     * Fetches the mongodb's collection object
+     * @param  {object} collection e.g. dpd.users
+     * @returns {Promise}
+     */
+    mongoCollection: function (collection) {
+        return collection.getResource().store.getCollection();
     }
 };
